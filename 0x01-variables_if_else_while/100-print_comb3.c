f64f376cb7782b7d6c338bb269bcf4a8f91a7247
Amr-3009 <amrosama1k@gmail.com>
2023-03-17 17:07:17 -0700

    ff

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,17 @@ int main(void)
 
 	for (i = 0; i <= 8; i++)
 	{
-	for (j = 1; j <= 9; j++)
+	for (j = i + 1; j <= 9; j++)
+	{
+	if (i != j)
 	{
 	putchar(i + 48);
 	putchar(j + 48);
-	if (i == 8 && j == 9)
-	{
-	break;
-	}
-	else
+	if (i < 8 && j <= 9)
 	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	}
