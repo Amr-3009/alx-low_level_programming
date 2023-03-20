771abdc0e4cea68b32a0d8aa115a7bfbebaa4bdf
Amr-3009 <amrosama1k@gmail.com>
2023-03-20 12:33:51 -0700

    kj

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: input
+ * Return: always
+ */
+
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10;
+	if (ld < 0)
+	{
+		ld = ld * -1;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
