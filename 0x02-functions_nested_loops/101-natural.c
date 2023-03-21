85648468088f49b834bd1422d9f62760a17171ae
Amr-3009 <amrosama1k@gmail.com>
2023-03-21 11:55:41 -0700

    gg

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum 
+ * Return: Always
+ */
+
+int main(void)
+
+{
+	int a, b = 0;
+
+	while (i < 1024)
+	{
+	if ((a % 3 == 0) || (a % 5 == 0))
+	{
+	b += a;
+	}
+	a++;
+	}
+	printf("%d\n", z);
+	return (0);
+
+}
