1d7f5e0cb227a901b7e9517a3f240f0e4c845625
Amr-3009 <amrosama1k@gmail.com>
2023-03-27 09:41:57 -0700

    gg

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values
+ * @a: input 1
+ * @b: input 2
+ * Return: always
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = temp;
+}
