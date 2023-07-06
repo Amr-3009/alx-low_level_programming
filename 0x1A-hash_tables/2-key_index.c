24e40a938c7615bc4cb250a66f6e25014c0d80d1
Amr-3009 <amrosama1k@gmail.com>
2023-07-06 10:46:39 -0700

    kk

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,18 +1,13 @@
-/**
- * hash_djb2 - Hash function, takes a string and turns it into a hash number
- * @str: the string, a constant value unrepeated in the hash table
- *
- * Return: The hash number
- */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
+#include "hash_tables.h"
 
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+/**
+ * key_index - gives the index of a key.
+ * @key: the key, a string.
+ * @size: the size of the array of the hash table.
+ *
+ * Return: Index number for the key/value pair in the hash table array.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
 }
